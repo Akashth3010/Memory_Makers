c7169fc15381061fad2ef8c07d377966e3a6c769
Rao <2464198@cognizant.com>
2025-12-26 17:17:43 +0530

    admin, host dashboard

--- TravelPackageManagementSystem/Controllers/HomeController.cs
+++ TravelPackageManagementSystem/Controllers/HomeController.cs
@@ -22,8 +22,12 @@ namespace TravelPackageManagementSystem.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Trending()
         public IActionResult Hero()
+        {
+            return View();
+        }
+
+        public IActionResult Host()
         {
             return View();
         }
