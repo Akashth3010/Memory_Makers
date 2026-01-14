3d9821c4c2111e03b88f063f157a8afa6b721ddd
Basani Anvith <2463599@cognizant.com>
2026-01-14 10:02:27 +0530

    deleted

--- TravelPackageManagementSystem/Program.cs
+++ TravelPackageManagementSystem/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+// ADD THIS LINE: This tells the app where AppDbContext is located
+using TravelPackageManagementSystem.Application.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register the Database Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -9,11 +17,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+// Note: In .NET 9, MapStaticAssets replaces UseStaticFiles for better performance
 app.UseRouting();
 
 app.UseAuthorization();
@@ -25,6 +33,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
-
