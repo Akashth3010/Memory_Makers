f4120956d26e5e9e000e2cdb775204424c11b80b
Akashth3010 <AkashHemalKumar.Thakkar@cognizant.com>
2026-01-17 00:16:51 +0530

    Fixing folder duplication and syncing namespaces

--- TravelPackageManagementSystem.Repository/Implementation/AuthModelRepository.cs
+++ TravelPackageManagementSystem.Repository/Implementation/AuthModelRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace TravelPackageManagementSystem.Repository.Implementation
+namespace TravelPackageManagementSystem.Repository.Implementations
 {
     internal class AuthModelRepository
     {
